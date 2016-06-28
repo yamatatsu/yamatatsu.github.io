b2140d1de51264bd58d8d18f3b75d486cf0a7405
yamatatsu <the.kinnikuman@gmail.com>
2016-06-28 22:58:24 +0900

    build js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-alert('app.js!!')
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+  <div>
+    Hello react!!
+  </div>,
+  document.getElementById('dom-root')
+)
